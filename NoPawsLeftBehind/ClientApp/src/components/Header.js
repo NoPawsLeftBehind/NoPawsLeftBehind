823a9325ff5be906ebb8fcf7f42a0baa7297c801
laolmstead <laolmstead19@gmail.com>
2021-10-21 03:00:28 -0400

    Update Landing page

--- NoPawsLeftBehind/ClientApp/src/components/Header.js
+++ NoPawsLeftBehind/ClientApp/src/components/Header.js
@@ -1,11 +1,21 @@
 ﻿import React, { Component } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { AppBar, Toolbar, Grid, Typography, Button, createTheme} from '@mui/material'
+import { Link } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import Logo from "../images/Logo.png";
+import '../style/Header.css';
 
-
-import './NavMenu.css';
+const appTheme = createTheme ({
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#7c4dff',
+        },
+        secondary: {
+            main: '#ff8a65',
+        },
+    },
+});
 
 export class Header extends Component {
     static displayName = Header.name;
@@ -17,27 +27,58 @@ export class Header extends Component {
     render() {
         return (
             <header>
-                <AppBar position="static">
-                    <Toolbar>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            No Paws Left Behind
-                        </Typography>
-                        <Button color="inherit">Login</Button>
-                        <Button color="inherit">Sign Up</Button>
-
-                    </Toolbar>
-                    <Toolbar>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            Home
-                        </Typography>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            Search Pets
-                        </Typography>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            Contact
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
+                <ThemeProvider theme={appTheme}>
+                    <AppBar position="static" className="app-bar">
+                        <Toolbar>
+                            <Grid container spacing={5}>
+                                <Grid item xs={2}>
+                                    <Link to="/">
+                                        <img src={Logo} className='logo-image' />
+                                    </Link>
+                                </Grid>
+                                <Grid item xs={1}>
+                                    <Link to="/">
+                                        <Typography variant="h6" component="div" className="path">
+                                            Home
+                                        </Typography>
+                                    </Link>
+                                </Grid>
+                                <Grid item xs={1}>
+                                    <Link to="/counter">
+                                        <Typography variant="h6" component="div" className="path">
+                                            About
+                                        </Typography>
+                                    </Link>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Link to="/fetch-data">
+                                        <Typography variant="h6" component="div" className="path">
+                                            Search Pets
+                                        </Typography>
+                                    </Link>
+                                </Grid>
+                                <Grid item xs={1}>
+                                    <Link to="/counter">
+                                        <Typography variant="h6" component="div" className="path">
+                                            Contact
+                                        </Typography>
+                                    </Link>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Link to="/fetch-data">
+                                        <Typography variant="h6" component="div" className="path">
+                                            FAQ
+                                        </Typography>
+                                    </Link>
+                                </Grid>
+                                <Grid item xs={3} className="sign-in-buttons">
+                                    <Button color="inherit" >Login</Button>
+                                    <Button color="inherit" >Sign Up</Button>
+                                </Grid>
+                            </Grid>
+                        </Toolbar>
+                    </AppBar>
+                </ThemeProvider>
             </header>
         );
     }
--- NoPawsLeftBehind/ClientApp/src/components/Home.js
+++ NoPawsLeftBehind/ClientApp/src/components/Home.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
+import { Grid, Typography, Box } from '@mui/material'
+import Logo from "../images/Logo.png";
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Home Page for No Paws Left Behind!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Box>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <img src={Logo} className="logo" />
+                        </Grid>
+                        <Grid item xs={6} className="summary">
+                            <Typography variant="h4" component="div">
+                                Save a pet. Be a superhero!
+                            </Typography>
+                            <Typography variant="p" component="div">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                                and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                                leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+                                the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+                                software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </div>
+        );
+    }
 }
