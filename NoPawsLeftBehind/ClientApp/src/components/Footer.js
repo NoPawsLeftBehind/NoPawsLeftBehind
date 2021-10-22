58481c4966a8b83d73270a294e2143260ba4706d
laolmstead <laolmstead19@gmail.com>
2021-10-22 00:34:27 -0400

    Update Landing Page

---
+++ NoPawsLeftBehind/ClientApp/src/components/Footer.js
@@ -0,0 +1,46 @@
+﻿import React, { Component } from 'react';
+import { AppBar, Paper, Toolbar, Typography } from '@mui/material'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <AppBar position="fixed" color="inherit" sx={{ top: 'auto', bottom: 0 }}>
+                    <Toolbar>
+                        <Grid container spacing={6}>
+                            <Grid item xs={2} />
+                            <Grid item xs={2}>
+                                <Link to="/">
+                                    <Typography variant="p" component="div" className="path">
+                                        Address
+                                    </Typography>
+                                </Link>
+                            </Grid>
+                            <Grid item xs={1}>
+                                |
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Link to="/counter">
+                                    <Typography variant="p" component="div" className="path">
+                                        Operating Hours
+                                    </Typography>
+                                </Link>
+                            </Grid>
+                            <Grid item xs={1}>
+                                |
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Link to="/fetch-data">
+                                    <Typography variant="p" component="div" className="path">
+                                        FAQ
+                                    </Typography>
+                                </Link>
+                            </Grid>
+                            <Grid item xs={2} />
+                        </Grid>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
--- NoPawsLeftBehind/ClientApp/src/components/Home.js
+++ NoPawsLeftBehind/ClientApp/src/components/Home.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react';
-import { Grid, Typography, Box } from '@mui/material'
+import { Grid, Paper, Typography } from '@mui/material'
 import Logo from "../images/Logo.png";
+import '../style/Home.css';
 
 export class Home extends Component {
     static displayName = Home.name;
 
     render() {
         return (
-            <div>
-                <Box>
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
+            <div className="mainDiv">
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <Paper elevation={3} className="paper">
                             <img src={Logo} className="logo" />
-                        </Grid>
-                        <Grid item xs={6} className="summary">
-                            <Typography variant="h4" component="div">
-                                Save a pet. Be a superhero!
-                            </Typography>
-                            <Typography variant="p" component="div">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                                and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                                leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-                                the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                                software like Aldus PageMaker including versions of Lorem Ipsum.
-                            </Typography>
-                        </Grid>
+                        </Paper>
                     </Grid>
-                </Box>
+                    <Grid item xs={6}>
+                        <Paper elevation={3} className="paper">
+                            <React.Fragment>
+                                <Typography variant="h4" component="div" className="summary">
+                                    Save a pet. Be a superhero!
+                                </Typography>
+                                <Typography variant="p" component="div" className="summary">
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+                                    the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+                                    software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </Typography>
+                            </React.Fragment>
+                        </Paper>
+                    </Grid>
+                </Grid>
             </div>
         );
     }
