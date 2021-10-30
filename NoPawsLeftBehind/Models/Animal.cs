f03e90adc74d02d8658550aa880d8dfc446c0680
JBPort94 <49971764+JBPort94@users.noreply.github.com>
2021-10-30 08:31:48 -0600

    Added to Animal.cs to hold all information we need for an Animal Object

--- NoPawsLeftBehind/Models/Animal.cs
+++ NoPawsLeftBehind/Models/Animal.cs
@@ -9,10 +9,18 @@ namespace NoPawsLeftBehind.Models
 {
     public class Animal
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
+        public string Picture { get; set; }
         public string Type { get; set; }
         public string Breed { get; set; }
         public string Gender { get; set; }
+        public string Availability { get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+        public string Color { get; set; }
+        public string Description { get; set; }
+        public string News { get; set; }
+        public DateTime DateCreated { get; set; }
     }
 }
