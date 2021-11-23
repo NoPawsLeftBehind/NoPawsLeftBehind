a4233348594d6bfdd445b920dc455013a2051fee
JBPort94 <49971764+JBPort94@users.noreply.github.com>
2021-11-23 14:43:02 -0700

    Creation of the object queries to populate lists from the db. Initial creation of the AddAnimalController to modify the get request.

---
+++ NoPawsLeftBehind/Controllers/AddAnimalController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using NoPawsLeftBehind.Database;
+using NoPawsLeftBehind.Models;
+using NoPawsLeftBehind.Queries;
+
+namespace NoPawsLeftBehind.Controllers
+{
+    [ApiController]
+    [Route("api/AddAnimal")]
+    public class AddAnimalController : ControllerBase
+    {
+        public AddAnimalController(AppDb db)
+        {
+            Db = db;
+        }
+
+        public AppDb Db { get; }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFilterOptions()
+        {
+            await Db.Connection.OpenAsync();
+            AnimalTypeQuery typeQuery = new AnimalTypeQuery(Db);
+            List<AnimalType> typeResult = await typeQuery.AllTypesAsync();
+
+            BreedQuery breedQuery = new BreedQuery(Db);
+            List<Breed> breedResult = await breedQuery.AllBreedsAsync();
+
+            DispositionQuery dispoQuery = new DispositionQuery(Db);
+            List<Disposition> dispoResult = await dispoQuery.AllDispositionsAsync();
+
+            FilterOptions filterOptions = new FilterOptions();
+
+            filterOptions.AnimalTypes = typeResult;
+            filterOptions.Breeds = breedResult;
+            filterOptions.Dispositions = dispoResult;
+
+            return new OkObjectResult(filterOptions);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostFilterResults([FromBody] FilterChoices choices)
+        {
+            await Db.Connection.OpenAsync();
+
+            AnimalQuery animalQuery = new AnimalQuery(Db);
+            var result = await animalQuery.SearchAnimalsAsync(choices);
+
+            return new OkObjectResult(result);
+        }
+    }
+}
---
+++ NoPawsLeftBehind/Queries/ColorQuery.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using NoPawsLeftBehind.Database;
+using NoPawsLeftBehind.Helpers;
+using NoPawsLeftBehind.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoPawsLeftBehind.Queries
+{
+    public class ColorQuery
+    {
+        internal AppDb Db { get; }
+
+        public ColorQuery(AppDb db)
+        {
+            Db = db;
+        }
+
+        public async Task<List<Color>> AllColorsAsync()
+        {
+
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"SELECT 	*
+                                FROM Colors";
+
+            return await ReadAllAsync(await cmd.ExecuteReaderAsync());
+        }
+
+        public async Task<List<Color>> ReadAllAsync(DbDataReader reader)
+        {
+            var colors = new List<Color>();
+
+            using (reader)
+            {
+                while (await reader.ReadAsync())
+                {
+                    var color = new Color()
+                    {
+                        colorID = reader.GetInt32(0),
+                        color = reader.GetString(1)
+                    };
+                    colors.Add(color);
+                }
+            }
+
+            return colors;
+        }
+    }
+}
