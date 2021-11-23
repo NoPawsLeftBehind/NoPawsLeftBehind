57547d53bf912cdbe37bec04c688f5c5e612d16b
JBPort94 <49971764+JBPort94@users.noreply.github.com>
2021-11-23 14:33:53 -0700

    Creating base objects for AddAnimal form population.

---
+++ NoPawsLeftBehind/Models/Sex.cs
@@ -0,0 +1,18 @@
+﻿using NoPawsLeftBehind.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoPawsLeftBehind.Models
+{
+    public class Sex
+    {
+        public Sex()
+        {
+        }
+
+        public int sexID { get; set; }
+        public string sex { get; set; }
+    }
+}
